938f81a494f20785ac53fc423362e4f64d7b68d8
truongquan-05 <quanctph50803@gmail.com>
2025-03-26 15:05:45 +0700

    base new

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Dang nhap";
 });
 
-Route::get('/test', [TestController::class, 'index']);
+
